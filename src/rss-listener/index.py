b4846d91c8baa477201170c0c90dbf26d50426b5
Misaka19614 <inzamzheng@icloud.com>
2023-11-26 16:49:11 +0800

    ✨ feat: 添加 rss 订阅函数，获取 rss 的更新信息并发送信息
    不需要任何入参，该函数一经触发会在 MongoDB 中读取相关配置信息然后调用 rss-subscribe
    函数没有返回值，获取的信息会输出到控制台，可在日志中查看

--- src/rss-listener/index.py
+++ src/rss-listener/index.py
@@ -2,8 +2,10 @@ import os
 import json
 import requests
 from pymongo import MongoClient
+from telebot import TeleBot
 
-def get_subscribed_rss_links():
+
+def get_subscribed_rss_links_dict():
     # 连接 MongoDB
     mongo_uri = os.environ.get("MONGO_URI")
     mongo_client = MongoClient(mongo_uri)
@@ -14,11 +16,23 @@ def get_subscribed_rss_links():
     # 获取 config 集合中的订阅链接
     config_collection = db.config
     config_document = config_collection.find_one({"type": "rss"})
-    
+
     if config_document:
-        return config_document.get("rss_links", [])
+        return config_document.get("subscribe_info", {})
     else:
-        return []
+        return {}
+
+
+def send_telegram_message(chat_id, text):
+    # 获取 Telegram Bot Token
+    telegram_bot_token = os.environ.get("TELEGRAM_BOT_TOKEN")
+
+    # 创建 TeleBot 实例
+    bot = TeleBot(telegram_bot_token)
+
+    # 发送消息
+    bot.send_message(chat_id, text, parse_mode="HTML", disable_web_page_preview=False)
+
 
 def main_handler(event, context):
     # 获取 MongoDB URI 和 RSS 订阅函数的 URL 从环境变量中
@@ -26,36 +40,44 @@ def main_handler(event, context):
     rss_function_url = os.environ.get("RSS_FUNCTION")
 
     # 获取订阅链接列表
-    rss_links = get_subscribed_rss_links()
+    subscribed_info = get_subscribed_rss_links_dict()
 
-    if not rss_links:
-        print("No RSS links found in the configuration.")
+    if not subscribed_info:
+        print("No RSS links subscribed.")
         return
 
-    # 构造 HTTP 请求体
-    request_body = {
-        "rss": rss_links
-    }
+    for chat_id, rss_links in subscribed_info.items():
+        # 构造 HTTP 请求体
+        request_body = {
+            "chat_id": chat_id,
+            # 用于测试
+            "force": "false",
+        }
 
-    # 构造 HTTP 请求头
-    headers = {
-        "Content-Type": "application/json"
-    }
+        # 构造 HTTP 请求头
+        headers = {"Content-Type": "application/json"}
 
-    # 调用 RSS 订阅函数
-    response = requests.post(rss_function_url, data=json.dumps(request_body), headers=headers)
+        # 调用 RSS 订阅函数
+        response = requests.post(
+            rss_function_url, data=json.dumps(request_body), headers=headers
+        )
 
-    # 解析 RSS 订阅函数的返回结果
-    rss_result = response.json()
+        # 解析 RSS 订阅函数的返回结果
+        rss_result = response.json()
+        if rss_result.get("errorCode"):
+            print(f"Error: {rss_result['errorMessage']}")
+            continue
 
-    # 输出描述新更新内容的文字
-    for rss_url, articles in rss_result.items():
-        if articles:
-            print(f"New updates from {rss_url}:")
-            for article in articles:
-                print(f"- {article['title']} ({article['link']})")
-        else:
-            print(f"No new updates from {rss_url}.")
+        # 输出描述新更新内容的文字
+        for rss_url, articles in rss_result.items():
+            if articles:
+                print(f"New updates from {rss_url}:")
+                for article in articles:
+                    message_text = f"New article: <b>{article['title']}</b>\nLink: {article['link']}"
+                    send_telegram_message(chat_id=chat_id, text=message_text)
+            else:
+                print(f"No new updates from {rss_url}.")
+
 
 if __name__ == "__main__":
     main_handler(None, None)  # 在本地测试时调用 main_handler 函数
