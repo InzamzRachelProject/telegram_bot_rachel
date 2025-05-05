d98261de3b9e7e859ac4046d819cb72990ea9570
InzamZheng <me@inzamz.top>
2025-05-05 14:07:14 +0800

    fix: fix from error

--- src/webhook-handler/index.py
+++ src/webhook-handler/index.py
@@ -315,7 +315,7 @@ def main_handler(event, context):
             
             client = MongoClient(os.getenv("MONGODB_ATLAS_URI"))
             db = client.get_database("ExtraCharactor")
-            collection_name = x["from"].split(maxsplit=1)[0] if " " in book_name else book_name
+            collection_name = x["from"].split(maxsplit=1)[0] if " " in x["from"] else x["from"]
             collection = db.get_collection(collection_name)
 
             # ========== 新增数据清洗逻辑 ==========
